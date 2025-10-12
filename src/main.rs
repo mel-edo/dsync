1eb737e26065ef17a5ecfd4233a9888ede8484f0
mel-edo <meledo@duck.com>
2025-10-12 18:43:43 +0530

    add clap for arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{collections::HashMap, fs::read, path::PathBuf, sync::Arc};
+use clap::Parser;
 
 use blake3::Hash;
 use tokio::sync::{mpsc, Mutex};
@@ -11,10 +12,32 @@ mod sync;
 mod util;
 mod watcher;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    // Folder to sync
+    #[arg(short = 'd', long, default_value = "./test_folder")]
+    path: String,
+
+    // Port to listen on
+    #[arg(short = 'p', long, default_value_t = 9000)]
+    port: u16,
+
+    // peer address
+    #[arg(short = 'a', long)]
+    peer: Option<String>,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let folder_path = PathBuf::from(args.path);
+    let port = args.port;
+    let peer_addr = args.peer;
+
     let (tx, mut rx) = mpsc::channel(100);
-    let _watcher = watch_folder(PathBuf::from("./test_folder"), tx.clone()).await?;
+    let _watcher = watch_folder(folder_path.clone(), tx.clone()).await?;
 
     let last_hashes: Arc<Mutex<HashMap<PathBuf, Hash>>> = Arc::new(Mutex::new(HashMap::<PathBuf, Hash>::new()));
     let last_hashes_clone: Arc<Mutex<HashMap<PathBuf, Hash>>> = Arc::clone(&last_hashes);
@@ -22,8 +45,9 @@ async fn main() -> anyhow::Result<()> {
     // start tcp server
     tokio::spawn({
         let tx_clone = tx.clone();
+        let folder_clone = folder_path.clone();
         async move {
-            if let Err(e) = network::start_server(9000, tx_clone).await {
+            if let Err(e) = network::start_server(port, folder_clone, tx_clone).await {
                 eprintln!("Server error: {:?}", e);
             }
         }
@@ -35,12 +59,13 @@ async fn main() -> anyhow::Result<()> {
             println!("Got FileEvent in main: {:?}", event);
 
             // don't forward loopback events
-            if event.file_path().to_string_lossy().contains("./test_folder") {
-                println!("Skipping loopback events {:?}", event.file_path());
-                continue;
-            }
+            // if event.file_path().starts_with(&folder_path) {
+                // println!("Skipping loopback events {:?}", event.file_path());
+                // continue;
+            // }
 
-            let hash = match read(&event.file_path()) {
+            let abs_path = folder_path.join(event.file_path());
+            let hash = match read(&abs_path) {
                 Ok(contents) => blake3::hash(&contents),
                 Err(_) => {
                     // file may be deleted for which we would have to delete it
@@ -58,8 +83,12 @@ async fn main() -> anyhow::Result<()> {
             }
             map.insert(event.file_path().clone(), hash);
 
-            if let Err(e) = network::send_event("127.0.0.1:9000", event).await {
-                eprintln!("Failed to send event: {:?}", e);
+            if let Some(ref peer) = peer_addr {
+                if let Err(e) = network::send_event(peer, event).await {
+                    eprintln!("Failed to send event: {:?}", e);
+                }
+            } else {
+                println!("No peer specified, skipping send");
             }
         }
     });
